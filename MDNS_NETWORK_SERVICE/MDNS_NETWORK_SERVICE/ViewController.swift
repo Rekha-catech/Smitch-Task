fc497ec8f414075cd7fb43e8ea9683ebb9949a35
Rekha suresh <rekha.suresh@catech.co.jp>
2022-09-22 16:18:55 +0530

    Initial Commit

---
+++ MDNS_NETWORK_SERVICE/MDNS_NETWORK_SERVICE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MDNS_NETWORK_SERVICE
+//
+//  Created by PQC India iMac-2 on 22/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
